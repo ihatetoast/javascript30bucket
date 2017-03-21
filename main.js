dcd8329d014905937917869b2c9f397125041eaa
Katy <ihatetoast@gmail.com>
2017-03-20 23:38:38 -0500

    added main js with notes for js fiddle faddle to do

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(e) { 
+  console.log("bucket is a go go");
+
+  // map over all the days (will need to add a class for all) to add addEventListener.
+
+  // when that square is clicked, let it close the bucket
+
+  // and then open another section that will have the thumbnail/link 
+  // and then bits of the code that i liked or was new to me
+  // and then (later) something I made from this (influenced by). 
+});
